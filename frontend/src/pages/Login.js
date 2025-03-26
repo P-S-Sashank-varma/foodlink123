8525f22d06729b0628740a44b7d628e41ff071fa
PATSAMATLA SAI SASHANK VARMA <163486858+P-S-Sashank-varma@users.noreply.github.com>
2025-03-26 22:04:18 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -13,7 +13,7 @@ const Login = ({ handleUserLogin }) => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://foodlink-1.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
